457945b55addb3bed10c95547f4dfcbb897eea6d
Aleksandr laptop <godlikedesigner@gmail.com>
2019-07-23 16:49:37 +0500

    add model and migration for uzbekistan

---
+++ database/migrations/2019_07_23_114409_create_uzbekistans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUzbekistansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uzbekistans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('category');
+            $table->string('sub-category')->nullable();
+            $table->string('image')->nullable();
+            $table->lognText('about');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uzbekistans');
+    }
+}
