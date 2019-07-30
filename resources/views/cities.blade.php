1b0f3daafdc836cd76e43a83a530138c76c3029f
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2019-07-31 00:03:55 +0500

    update cards in city view

--- resources/views/cities.blade.php
+++ resources/views/cities.blade.php
@@ -4,31 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!--===============================================================================================-->
+        <link rel="icon" type="image/png" href="/images/icons/favicon.png"/>
     <!--===============================================================================================-->
-    <!-- Style CSS -->
-    <link rel="stylesheet" href="/css/style-blog.css">
-   <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="///images/icons/favicon.png"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="/fonts/fontawesome-5.0.8/css/fontawesome-all.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/vendor/animsition/css/animsition.min.css">
-<!--==============================================/=================================================-->
-	<link rel="stylesheet" type="text/css" href="/css/util.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="/css/card.css">    <link rel="stylesheet" type="text/css" href="/css/main-menu.css">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/fonts/fontawesome-5.0.8/css/fontawesome-all.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <link rel="stylesheet" href="/css/tour-all.css">
+    <!--===============================================================================================-->
+    <script src="https://kit.fontawesome.com/cdcf5aa2f7.js"></script>
+    <link rel="stylesheet" type="text/css" href="/css/card.css"> 
+    <link rel="stylesheet" type="text/css" href="/css/main-menu.css">
+    <link rel="stylesheet" type="text/css" href="/css/main-page.css">
 </head>
 <body>
     @include('/components/header', ['type' => 'city'])
@@ -38,21 +29,30 @@
     font-weight: 700;
     color: #0460d9;" href="/city/{{Illuminate\Support\Str::lower($city->name)}}">{{$city->name}}</a></div>
         <div class="row container_uzb" >
-                <a class="col-md-4" href="{{route('city.category', ['city'=> Illuminate\Support\Str::lower($city->name),'category'=>'history'])}}">
-                    <div class="a_card card_big" style="background-size: cover;">
-                            <p>История</p>
-                    </div>
-                </a>
-                <a class="col-md-4" href="{{route('city.category', ['city'=> Illuminate\Support\Str::lower($city->name),'category'=>'what-to-do'])}}">
-                    <div class="a_card card_big" style="background-size: cover;">
-                        <p>Чем занятся</p>
-                    </div>
-                </a> 
-                <a class="col-md-4" href="">
-                        <div class="a_card card_big" style="background-size: cover;">
-                            <p>Что смотреть</p>
+                <article class="col-md-4 block_main__item" style="margin-top:50px;">
+                        <div class="content-block__img">
+                            <a href="{{route('city.category', ['city'=> Illuminate\Support\Str::lower($city->name),'category'=>'history'])}}"></a>
                         </div>
-                </a> 
+                        <div class="content-block__title">
+                            <a href="{{route('city.category', ['city'=> Illuminate\Support\Str::lower($city->name),'category'=>'history'])}}">История</a>
+                        </div>
+                </article>
+                <article class="col-md-4 block_main__item" style="margin-top:50px;">
+                        <div class="content-block__img">
+                            <a href="{{route('city.category', ['city'=> Illuminate\Support\Str::lower($city->name),'category'=>'what-to-do'])}}"></a>
+                        </div>
+                        <div class="content-block__title">
+                            <a href="{{route('city.category', ['city'=> Illuminate\Support\Str::lower($city->name),'category'=>'what-to-do'])}}">Чем занятся</a>
+                        </div>
+                </article>
+                <article class="col-md-4 block_main__item" style="margin-top:50px;">
+                        <div class="content-block__img">
+                            <a href="{{route('city.category', ['city'=> Illuminate\Support\Str::lower($city->name),'category'=>'what-to-do'])}}"></a>
+                        </div>
+                        <div class="content-block__title">
+                            <a href="{{route('city.category', ['city'=> Illuminate\Support\Str::lower($city->name),'category'=>'what-to-do'])}}">Что смотреть<</a>
+                        </div>
+                </article>
                 <a href="/city/{{Illuminate\Support\Str::lower($city->name)}}" style="margin: auto; margin-top: 20px;" class="btn btn-primary">Подробнее</a>
         </div>
         @endforeach
