d6c7c485ffbe24f9ea3ace7d3bb5077df03012b8
Aleksandr laptop <godlikedesigner@gmail.com>
2019-07-31 12:09:33 +0500

    update cards for view city category

--- resources/views/city-category.blade.php
+++ resources/views/city-category.blade.php
@@ -5,170 +5,87 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!--===============================================================================================-->
-    <!-- Style CSS -->
-    <link rel="stylesheet" href="/css/style-blog.css">
-   <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="///images/icons/favicon.png"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="/fonts/fontawesome-5.0.8/css/fontawesome-all.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/vendor/animsition/css/animsition.min.css">
-<!--==============================================/=================================================-->
-	<link rel="stylesheet" type="text/css" href="/css/util.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/css/card.css">    
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-    <link rel="stylesheet" href="/css/slick/custom-theme.css">
-    <style>
-    .sub-category{
-        font-size: 35px;
-        font-weight: 700;
-        margin-top: 50px;
-    }
-    </style>
+           <link rel="stylesheet" type="text/css" href="/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/fonts/fontawesome-5.0.8/css/fontawesome-all.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <link rel="stylesheet" href="/css/tour-all.css">
+    <!--===============================================================================================-->
     <script src="https://kit.fontawesome.com/cdcf5aa2f7.js"></script>
+    <link rel="stylesheet" type="text/css" href="/css/card.css"> 
     <link rel="stylesheet" type="text/css" href="/css/main-menu.css">
+    <link rel="stylesheet" type="text/css" href="/css/main-page.css">
     <title>Document</title>
 </head>
 <body>
     @include('/components/header', ['type' => 'city'])
     <div class="container">
-            
-        <nav class="category_nav col-md-12" style="margin-top:50px;">
-                    <ul class="row" style="    justify-content: space-between;"> 
-                        <li>
-                            <a href=""><i class="fas fa-history"></i> История</a>
-                        </li>
-                        <li>
-                            <a href=""><i class="fas fa-mosque"></i> Что смотреть</a>
-                        </li>
-                        <li>
-                            <a href=""><i class="fas fa-tree"></i> Чем заняться</a>
-                        </li>
-                        <li>
-                            <a href=""><i class="fas fa-shopping-cart"></i> Где покупать</a>
-                        </li>
-                        <li>
-                            <a href=""><i class="fas fa-utensils"></i> Где поесть</a>
-                        </li>
-                        <li>
-                            <a href=""><i class="fas fa-hotel"></i> Где остановиться</a>
-                        </li>
-                        <li>
-                            <a href=""><i class="fas fa-taxi"></i> Как добраться</a>
-                        </li>
-                        <li>
-                            <a href=""><i class="fas fa-info"></i> Полезная информация</a>
-                        </li>
-                    </ul>
-                </nav>
         <div class="col-md-10" style="padding:0;">
             <div class="row">
                 @if($items[0]->category_price)
-                    <div class="col-md-12 sub-category">Дешевый</div>
+                    <div class="col-md-12 sub-category" style="font-size:25px; margin-top:50px; margin-bottom:20px;">Дешевый</div>
                     @foreach($items as $item)
                         @if($item->category_price === 'cheap')
-                            <a class="col-md-4 main_b_card " href="{{Request::url()}}/{{$item->slug}}">
-                                <div class="b_card b_card_category" style="background: url(/storage/{{$item->image}});     background-size: cover;">    
-                                    <div class="b_card_bottom">
-                                        <div class="b_card_name">{{$item->name}}</div>
-                                        <div class="b_map-controller">
-                                            <span><i class="fas fa-dollar-sign"></i>
-                                                @if($item->category_price === 'cheap')
-                                                    Дешевый
-                                                @elseif($item->category_price === 'middle')
-                                                    Средний
-                                                @elseif($item->category_price === 'expensive')
-                                                    Элитный
-                                                @endif
-                                            </span> 
-                                            <i class="fas fa-arrow-right"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
+                        <article class="col-md-4 block_main__item">
+                            <div class="content-block__img"><img src="/storage/{{$item->image}}" alt=""></div>
+                            <div class="content-block__category">
+                                @if($item->category_price === 'cheap')
+                                    Дешевый
+                                @elseif($item->category_price === 'middle')
+                                    Средний
+                                @elseif($item->category_price === 'expensive')
+                                    Элитный
+                                @endif
+                            </div>
+                            <div class="content-block__title"><a  href="{{Request::url()}}/{{$item->slug}}">{{$item->name}}</a></div>
+                        </article>
                         @endif
                     @endforeach
-                    <div class="col-md-12 sub-category">Средний</div>
+                    <div class="col-md-12 sub-category" style="font-size:25px; margin-top:50px; margin-bottom:20px;">Средний</div>
                     @foreach($items as $item)
                         @if($item->category_price === 'middle')
-                            <a class="col-md-4 main_b_card " href="{{Request::url()}}/{{$item->slug}}">
-                                <div class="b_card b_card_category" style="background: url(/storage/{{$item->image}});     background-size: cover;">    
-                                    <div class="b_card_bottom">
-                                        <div class="b_card_name">{{$item->name}}</div>
-                                        <div class="b_map-controller">
-                                            <span><i class="fas fa-dollar-sign"></i>
-                                                @if($item->category_price === 'cheap')
-                                                    Дешевый
-                                                @elseif($item->category_price === 'middle')
-                                                    Средний
-                                                @elseif($item->category_price === 'expensive')
-                                                    Элитныый
-                                                @endif
-                                            </span> 
-                                            <i class="fas fa-arrow-right"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
+                        <article class="col-md-4 block_main__item">
+                            <div class="content-block__img"><img src="/storage/{{$item->image}}" alt=""></div>
+                            <div class="content-block__category">
+                                @if($item->category_price === 'cheap')
+                                    Дешевый
+                                @elseif($item->category_price === 'middle')
+                                    Средний
+                                @elseif($item->category_price === 'expensive')
+                                    Элитный
+                                @endif
+                            </div>
+                            <div class="content-block__title"><a  href="{{Request::url()}}/{{$item->slug}}">{{$item->name}}</a></div>
+                        </article>
                         @endif
                     @endforeach
-                    <div class="col-md-12 sub-category">Дорогой</div>
+                    <div class="col-md-12 sub-category" style="font-size:25px; margin-top:50px; margin-bottom:20px;">Дорогой</div>
                     @foreach($items as $item)
                         @if($item->category_price === 'expensive')
-                        <a class="col-md-4 main_b_card " href="{{Request::url()}}/{{$item->slug}}">
-                                <div class="b_card b_card_category" style="background: url(/storage/{{$item->image}});     background-size: cover;">    
-                                    <div class="b_card_bottom">
-                                        <div class="b_card_name">{{$item->name}}</div>
-                                        <div class="b_map-controller">
-                                            <span><i class="fas fa-dollar-sign"></i>
-                                                @if($item->category_price === 'cheap')
-                                                    Дешевый
-                                                @elseif($item->category_price === 'middle')
-                                                    Средний
-                                                @elseif($item->category_price === 'expensive')
-                                                    Дорогой
-                                                @endif
-                                            </span> 
-                                            <i class="fas fa-arrow-right"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
+                        <article class="col-md-4 block_main__item">
+                            <div class="content-block__img"><img src="/storage/{{$item->image}}" alt=""></div>
+                            <div class="content-block__category">
+                                @if($item->category_price === 'cheap')
+                                    Дешевый
+                                @elseif($item->category_price === 'middle')
+                                    Средний
+                                @elseif($item->category_price === 'expensive')
+                                    Элитный
+                                @endif
+                            </div>
+                            <div class="content-block__title"><a  href="{{Request::url()}}/{{$item->slug}}">{{$item->name}}</a></div>
+                        </article>
                         @endif
                     @endforeach
                 @else
                     @foreach($items as $item)
-                        <a class="col-md-4 main_b_card " href="{{Request::url()}}/{{$item->id}}">
-                                <div class="b_card b_card_category" style="background: url(/storage/{{$item->image}});     background-size: cover;">    
-                                    <div class="b_card_bottom">
-                                        <div class="b_card_name">{{$item->name}}</div>
-                                        <div class="b_map-controller">
-                                            <span><i class="fas fa-dollar-sign"></i>
-                                                @if($item->category_price === 'cheap')
-                                                    Дешевый
-                                                @elseif($item->category_price === 'middle')
-                                                    Средний
-                                                @elseif($item->category_price === 'expensive')
-                                                    Дорогой
-                                                @endif
-                                            </span> 
-                                            <i class="fas fa-arrow-right"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
+                        <article class="col-md-4 block_main__item">
+                            <div class="content-block__img"><img src="/storage/{{$item->image}}" alt=""></div>
+                            <div class="content-block__title"><a  href="{{Request::url()}}/{{$item->slug}}">{{$item->name}}</a></div>
+                        </article>
                     @endforeach
                 @endif
             </div>
