cf7780dc5bacbf09abeddcf7aa2452f85b13419f
Aleksandr laptop <godlikedesigner@gmail.com>
2019-08-02 18:44:39 +0500

    add new faq and team page

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--===============================================================================================-->
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="/css/style-blog.css">
+   <!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="///images/icons/favicon.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="/fonts/fontawesome-5.0.8/css/fontawesome-all.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/vendor/animsition/css/animsition.min.css">
+<!--==============================================/=================================================-->
+	<link rel="stylesheet" type="text/css" href="/css/util.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/css/card.css">    
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" href="/css/slick/custom-theme.css">
+    <link rel="stylesheet" type="text/css" href="/css/main-menu.css">
+    <title>Document</title>
+</head>
+<body>
+@include('/components/header')
+    <div class="container">
+        <div class="col-md-12">
+            <div class="row">
+                <div style="padding-bottom: 20px; padding-top: 20px;" class="f2-s-1 p-r-30 m-tb-6">
+                    <a href="/" class="breadcrumb-item f1-s-3 cl9">
+                        Главная 
+                    </a>
+                    <span class="breadcrumb-item f1-s-3 cl9">
+                        FAQ
+                    </span>	
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
