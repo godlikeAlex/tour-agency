b46c24977bea8c484321af2806a7b4f419f3ad04
Aleksandr laptop <godlikedesigner@gmail.com>
2019-08-22 14:03:20 +0500

    added css in tour page for menu

--- resources/views/tours.blade.php
+++ resources/views/tours.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" type="text/css" href="/vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/main-page.css">
     <link rel="stylesheet" href="/css/main-menu.css">
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
     <style>
         .main-content-block {
             border-bottom: none;
